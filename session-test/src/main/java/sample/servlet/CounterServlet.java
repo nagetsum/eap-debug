5b856c71bbc74d71d96024b3d7232036b289c2e2
Norito Agetsuma <norito.agetsuma@gmail.com>
2018-12-02 14:36:30 +0900

    show hostname

--- session-test/src/main/java/sample/servlet/CounterServlet.java
+++ session-test/src/main/java/sample/servlet/CounterServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @WebServlet("/count")
@@ -24,6 +25,6 @@ public class CounterServlet extends HttpServlet {
         session.setAttribute("counter", counter);
 
         res.setContentType("text/plain; charset=utf-8");
-        res.getWriter().write("count: " + now);
+        res.getWriter().write("count: " + now + ", host: " + InetAddress.getLocalHost().getHostName());
     }
 }
