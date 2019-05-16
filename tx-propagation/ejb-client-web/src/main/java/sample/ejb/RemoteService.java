7269fb89ecbe35b8c8a55a43f412844cc0267c10
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-05-16 10:20:06 +0900

    try to disable transaction context propagation

--- tx-propagation/ejb-client-web/src/main/java/sample/ejb/RemoteService.java
+++ tx-propagation/ejb-client-web/src/main/java/sample/ejb/RemoteService.java
@@ -2,8 +2,11 @@ package sample.ejb;
 
 import javax.ejb.Remote;
 import java.io.Serializable;
+import org.jboss.ejb.client.annotation.ClientTransaction;
+import org.jboss.ejb.client.annotation.ClientTransactionPolicy;
 
 @Remote
+@ClientTransaction(ClientTransactionPolicy.NOT_SUPPORTED)
 public interface RemoteService extends Serializable {
     void credit(int toAccountId, int amount);
 }
