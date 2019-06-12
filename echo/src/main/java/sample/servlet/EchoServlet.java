982fa9f35c90fbbd19abb1407ca420d18c7d7c98
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-06-12 16:57:21 +0900

    remove debug code

--- echo/src/main/java/sample/servlet/EchoServlet.java
+++ echo/src/main/java/sample/servlet/EchoServlet.java
@@ -13,6 +13,5 @@ public class EchoServlet extends HttpServlet {
             throws IOException {
         res.setContentType("text/plain; charset=utf-8");
         res.getWriter().write("echo");
-        System.out.println(System.getProperty("file.encoding"));
     }
 }
