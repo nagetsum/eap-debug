58dae5ba2b80b18d0aae8e71d52bbcd5e05894dd
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-07-23 22:09:41 +0900

    disable jboss logging

--- echo/src/main/java/sample/servlet/EchoServlet.java
+++ echo/src/main/java/sample/servlet/EchoServlet.java
@@ -6,20 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import org.jboss.logging.Logger;
-
-
 @WebServlet("/echo")
 public class EchoServlet extends HttpServlet {
-
-    static Logger logger = Logger.getLogger(EchoServlet.class);
-
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res)
             throws IOException {
         res.setContentType("text/plain; charset=utf-8");
         res.getWriter().write("echo");
-        System.out.println("test print");
-        logger.info("test message");
     }
 }
