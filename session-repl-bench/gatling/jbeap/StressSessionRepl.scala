d245eea0ad99209a39f96c03ac35e979ad59ee1c
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-07-26 15:55:53 +0900

    polish

--- session-repl-bench/gatling/jbeap/StressSessionRepl.scala
+++ session-repl-bench/gatling/jbeap/StressSessionRepl.scala
@@ -13,21 +13,23 @@ class StressSessionRepl extends Simulation {
     .acceptEncodingHeader("gzip, deflate, br")
     .userAgentHeader("User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
 
-  val scn = scenario("StressSessionRepl") 
-    .repeat(1000) {
-      exec(
-        http("session")
-          .get("/session")
-          .check(status.is(200))
-      )
+  val scn = scenario("StressSessionRepl")
+    .during(120) {   // exec during 120 seconds per a user
+      repeat(100) {  // call continuously http...//session-repl-bench/session 100 times without interval
+        exec(
+          http("session")
+            .get("/session")
+            .check(status.is(200))
+        )
+      }
+      .exec(          // call http...//session-repl-bench/invalidate after caling /session 100 times
+        http("invalidate")
+          .get("/invalidate")
+          .check(status.is(200)))
+      .pause(1)      // then sleep 1 second
     }
-    .exec(
-      http("invalidate")
-        .get("/invalidate")
-        .check(status.is(200)))
-    .pause(1)
 
   setUp(
-    scn.inject(rampUsers(1000) during (1 minutes))
-  ).protocols(httpProtocol).maxDuration(1 minutes)
+    scn.inject(rampUsers(1000) during (10 seconds)) // concurrent access users 1000, ramp-up time 10 sec
+  ).protocols(httpProtocol)
 }
